3951176991d7808539e9e9ebbc7000c001da8409
Admin <admin@mac-admin.local>
2018-06-04 21:13:50 +0300

    commit created screen

---
+++ Driveo/Screens/CreateRequestScreen/CreateRequestPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  CreateRequestPresenter.swift
+//  Driveo
+//
+//  Created by Admin on 6/4/18.
+//  Copyright © 2018 ITI. All rights reserved.
+//
+
+import Foundation
+class CreateRequestPresenter{
+    
+}
