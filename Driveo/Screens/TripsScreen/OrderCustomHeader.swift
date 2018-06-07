aa80702f997b47d73ed6d7bb3d9b3d0ae0f344b8
3alilio <35105321+3alilio@users.noreply.github.com>
2018-06-08 01:21:21 +0300

    Add header and shadow (not ready yet)

---
+++ Driveo/Screens/TripsScreen/OrderCustomHeader.swift
@@ -0,0 +1,27 @@
+//
+//  OrderCustomHeader.swift
+//  Driveo
+//
+//  Created by Admin on 6/8/18.
+//  Copyright © 2018 ITI. All rights reserved.
+//
+
+import UIKit
+
+class OrderCustomHeader: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
