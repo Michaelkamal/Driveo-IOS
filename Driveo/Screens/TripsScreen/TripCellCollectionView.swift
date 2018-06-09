346241ca10754d1428e5438c544fc9158855bda2
3alilio <35105321+3alilio@users.noreply.github.com>
2018-06-09 14:48:33 +0300

    add collection view instead of table view in orders

---
+++ Driveo/Screens/TripsScreen/TripCellCollectionView.swift
@@ -0,0 +1,27 @@
+//
+//  TripCellCollectionView.swift
+//  Driveo
+//
+//  Created by Admin on 6/9/18.
+//  Copyright © 2018 ITI. All rights reserved.
+//
+
+import UIKit
+
+class TripCellCollectionView: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var orderStatusLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet weak var paymentImage: UIImageView!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+}
