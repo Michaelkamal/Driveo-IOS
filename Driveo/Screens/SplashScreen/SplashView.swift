2a4c81b02a5851a83cacd1c251ae9223a991230b
3alilio <35105321+3alilio@users.noreply.github.com>
2018-06-16 21:32:35 +0300

    change Suffix url for history Trips and change root controller in Splash

--- Driveo/Screens/SplashScreen/SplashView.swift
+++ Driveo/Screens/SplashScreen/SplashView.swift
@@ -29,28 +29,17 @@ class SplashView: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5), execute: {
             self.LoadingSpinner.stopAnimating()
             let defaults = UserDefaults.standard
-            if defaults.string(forKey: "auth_token") != nil {
-                if defaults.string(forKey: "verified") != nil {
+            if defaults.string(forKey: "auth_token") != nil && defaults.string(forKey: "verified") != nil {
                     let sourceScreenStoryboard = UIStoryboard(name: ScreenController.main.storyBoardName(), bundle: nil)
                     let home = sourceScreenStoryboard.instantiateViewController(withIdentifier: ScreenController.main.rawValue.trimmingCharacters(in: CharacterSet.whitespaces))
-                    
-                    self.present(home, animated: true, completion: nil)
-                    
-                  //  UIApplication.shared.keyWindow?.rootViewController = home
-                    
+                  //  self.present(home, animated: true, completion: nil)
+                    UIApplication.shared.keyWindow?.rootViewController = home
                 }
-                else {
-                        let verifyStoryBoard = UIStoryboard.init(name:"SignupStoryboard",bundle:nil)
-                        let verifyScreen = verifyStoryBoard.instantiateViewController(withIdentifier: "VerifyView") as! VerifyView
-                    //  UIApplication.shared.keyWindow?.rootViewController = signup
-                        self.present(verifyScreen, animated: true, completion: nil)
-                    
-                  
-                }
-            } else {
+            else {
                 let loginStoryBoard = UIStoryboard.init(name: "Login", bundle: nil)
                 let loginPage:LoginViewController = loginStoryBoard.instantiateViewController(withIdentifier: "loginView") as! LoginViewController
-                self.present(loginPage, animated: true, completion: nil)
+             //   self.present(loginPage, animated: true, completion: nil)
+                   UIApplication.shared.keyWindow?.rootViewController = loginPage
             }
         })
         
